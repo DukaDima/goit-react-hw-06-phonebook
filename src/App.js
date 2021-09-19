82201a7f5418165c39b68652337d060457f48865
Dima <75967292+DukaDima@users.noreply.github.com>
2021-09-19 21:43:58 +0300

    Local storage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import React, { useState, useEffect } from 'react';
 import ContactsList from './components/ContactsList/ContactsList';
 import AddContacts from './components/AddContacts/AddContacts';
 import Filter from './components/Filter/Filter';
 import Title from './components/Title/Title';
-// import shortid from 'shortid';
 import PropTypes from 'prop-types';
 
 export default function App() {
-  const [contacts, setContacts] = useState(
-    JSON.parse(window.localStorage.getItem('contacts')) ?? [],
-  );
-
-  // contacts.find(elem => elem.name === contact.name)
-  //       ? alert(`${contact.name} is already in contacts`)
-  //       : setContacts(oldContacts => [contact, ...oldContacts]);
-  //   };
-
-  useEffect(() => {
-    window.localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
   return (
     <>
       <h1>Телефонная книга</h1>
